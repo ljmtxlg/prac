8b3f04857056cd752e805d5578dfe2ced73075eb
ljmtxlg <ljmtxlg@gmail.com>
2013-09-29 19:12:24 +0800

    add kmp

---
+++ src/com/practice/KMP.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+public class KMP {
+    public int kmp(String s, String p) {
+        
+        return 0;
+    }
+    
+    public void getNextStep(String p, int[] nextStep) {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
